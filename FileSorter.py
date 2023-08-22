341871e90e1daf1d67981045f891899b329e77d6
yalonschori <yalonofficial@gmail.com>
2023-08-22 13:57:56 +0300

    Initial Commit for File Sorter App

---
+++ FileSorter.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+filetypes = {
+    "RAR": ".rar",
+    "JPG": ".jpg",
+    "PNG": ".png",
+    "Text Documents": ".txt",
+    "PDF": ".pdf",
+}
+
+
+def SortFolders():
+    path = "C:/Users/yalon/Desktop/Messy/"
+    os.chdir(r"C:/Users/yalon/Desktop/Messy")
+
+    messyItems = os.listdir(path)
+
+    for i in filetypes:
+        for item in messyItems:
+            if item.endswith(filetypes[i]):
+                if os.path.exists(path + i):
+                    shutil.move(item, path + i)
+                else:
+                    os.mkdir(path + i)
+                    shutil.move(item, path + i)
+
+
+SortFolders()
